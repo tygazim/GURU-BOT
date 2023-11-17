ff61e537643cad712f31b75fc974df443384db95
tygazim <90338142+tygazim@users.noreply.github.com>
2023-11-17 05:54:00 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['263783876438', 'METISHA', true],
+  ['263784807266', 'TYGA SPARTA', true], 
   [''] 
 ] //Number of owners
 
